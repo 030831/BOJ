fc4ece3e7352082d1a8d555693076db88ac5c6cf
030831 <101914089+030831@users.noreply.github.com>
2023-01-15 11:14:39 +0900

    [Bronze II] Title: 초6 수학, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2702. 초6 수학/초6 수학.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int c = a%b;
+    while (c!=0)
+    {
+        a=b;
+        b=c;
+        c=a%b;
+    }
+
+    return b;
+}
+
+int lcm(int a, int b)
+{
+    return (a*b)/gcd(a,b);
+}
+int main()
+{
+    int N ; 
+
+    cin >> N;
+
+    int a,b;
+
+    for (int i = 0 ; i < N ; i ++)
+    {
+        cin >> a >> b;
+
+        cout << lcm(a,b) << " " << gcd(a,b) << endl;
+
+    }
+}
