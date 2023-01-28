8271621cd0618545abb824b43d299ac41d1a15ad
030831 <101914089+030831@users.noreply.github.com>
2023-01-28 12:22:29 +0900

    [Silver I] Title: 트리 순회, Time: 108 ms, Memory: 241112 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,35 +1,34 @@
 import sys
 sys.setrecursionlimit(10**5)
-N=int(input())
-
-Tree={}
-
-for i in range(N):
-    a,b,c=input().split()
-    Tree[a]=[b,c]
-
 
 def preorder(node):
 
     if node!=".":
-        print(node,end="")
+        print(node , end="")
         preorder(Tree[node][0])
         preorder(Tree[node][1])
 
-
 def inorder(node):
-
     if node!=".":
         inorder(Tree[node][0])
-        print(node, end="")
+        print(node ,end="")
         inorder(Tree[node][1])
 
 def postorder(node):
-
     if node!=".":
         postorder(Tree[node][0])
         postorder(Tree[node][1])
-        print(node,end="")
+        print(node , end="")
+
+
+N=int(input())
+Tree={}
+
+for i in range(N):
+    parent,left_node,right_node=input().split()
+
+    Tree[parent]=[left_node,right_node]
+
 
 preorder('A')
 print()
