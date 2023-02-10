f6a8d3ff09cc790566117456537b3ee6b36ee036
030831 <101914089+030831@users.noreply.github.com>
2023-02-10 19:59:49 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 80 ms, Memory: 34308 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+
+def BFS():
+
+    while deq:
+        x,total=deq.popleft()
+
+        if x==100:
+            return total
+
+        for i in range(1,7):
+            nx=x+i
+            if nx<=100 and not visit[nx]:
+                check = False
+                for j in range(N):
+                    if N_list[j][0] == nx:
+                        deq.append([N_list[j][1], total + 1])
+                        visit[N_list[j][1]] = True
+                        check=True
+                for j in range(M):
+                    if M_list[j][0] == nx:
+                        deq.append([M_list[j][1], total + 1])
+                        visit[M_list[j][1]] = True
+                        check=True
+                if not check:
+                    visit[nx]=True
+                    deq.append([nx,total+1])
+
+
+N,M=map(int,input().split())
+
+deq=deque() ;  deq.append((0,0)) ; deq.append((1,0))
+
+visit=[False]*101
+N_list=[ list(map(int,input().split())) for _ in range(N) ]
+M_list=[ list(map(int,input().split())) for _ in range(M) ]
+
+print( BFS() )
