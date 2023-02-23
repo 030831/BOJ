d1500b6e15a1dd42738c52c3b1a6745097b2aae0
030831 <101914089+030831@users.noreply.github.com>
2023-02-23 12:24:47 +0900

    [Platinum I] Title: 이동, Time: 72 ms, Memory: 17244 KB -BaekjoonHub

---
+++ 백준/Platinum/1067. 이동/이동.cc
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef complex<double> cpx; // 복소수
+const double PI = acos(-1); // PU 값
+
+#define MAX 200001 // 최대값
+int N,M,K,P,answer,bit;
+
+void FFT(vector<cpx> &f , bool inv)
+{
+    P = f.size();
+
+    for (int i = 1 ,j = 0; i < P ; i++)
+    {
+        bit = P>>1;
+
+        while (j>=bit)
+        {
+            j-=bit;
+            bit/=2;
+        }
+
+        j+=bit;
+
+        if (i<j)
+        {
+            swap(f[i] , f[j]);
+        }
+    }
+
+    for (int i = 1 ; i < P ; i <<=1)
+    {
+        double x = inv ? PI/i : -PI/i;
+        cpx w(cos(x) , sin(x));
+
+        for (int j = 0 ; j < P ; j+= i<<1)
+        {
+            cpx p = cpx(1,0);
+
+            for (int k = 0 ; k < i ; k++)
+            {
+                cpx tmp = f[i+j+k] * p;
+                f[i+j+k] = f[j+k] - tmp;
+                f[j+k] +=tmp;
+                p *= w;
+            }
+        }
+
+    }
+
+    if (inv) 
+    {
+        for (int i = 0 ; i < P ; i ++)
+        {
+            f[i]/=P;
+        }
+    }
+}
+
+vector<cpx> multiply(vector<cpx> &a , vector<cpx> &b) 
+{
+    P = 1;
+
+    while (P<a.size() + b.size())
+    {
+        P*=2;
+    }
+
+    a.resize(P);
+    FFT(a,false);
+    b.resize(P);
+    FFT(b,false);
+     // FFT를 통해 두 다항식을 DFT해 둔다.
+
+    vector<cpx> c(P);
+    for (int i = 0 ; i < P ; i ++)
+    {
+        c[i] = a[i]*b[i];
+        // DFT한 값들끼리 곱하면 결과 다항식의 DFT값이 된다.
+    }
+    FFT(c, true);
+    //역변환
+
+    return c;
+
+}
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> N;
+
+    vector<int> A(N+N) ,B(N);
+
+    for (int i = 0 ; i < N ; i++)
+    {
+        cin >> A[i];
+    }
+
+    for (int i =N-1 ; i>=0 ; i--)
+    {
+        cin >> B[i];
+    }
+
+    for (int i = 0 ; i<N ; i++)
+    {
+        A[i+N]=A[i];
+    }
+
+    vector<cpx> a,b;
+    for (auto i : A)
+    {
+        a.push_back(cpx(i,0));
+    }
+    for (auto i : B)
+    {
+        b.push_back(cpx(i,0));
+    }
+
+    vector<cpx> c = multiply(a,b);
+
+    long long answer = 0;
+
+    for (int i = 0 ; i<c.size() ; i++)
+    {
+        answer = max<long long>(answer , round(c[i].real()));
+    }
+
+    cout << answer;
+}
