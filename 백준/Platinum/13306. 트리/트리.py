24c4e1a336cace2ded35a811fc3c92ac1e6fa7b0
030831 <101914089+030831@users.noreply.github.com>
2023-03-15 18:05:33 +0900

    [Platinum IV] Title: 트리, Time: 400 ms, Memory: 153892 KB -BaekjoonHub

--- 백준/Platinum/13306. 트리/트리.py
+++ 백준/Platinum/13306. 트리/트리.py
@@ -3,8 +3,7 @@ import sys
 input = sys.stdin.readline
 
 N, Q = map(int, input().split())
-parent = [i for i in range(N + 1)]
-
+parent = [i for i in range(N+1)]
 
 def find(x):
     if x != parent[x]:
@@ -15,15 +14,13 @@ def find(x):
 def union(x, y):
     x = find(x)
     y = find(y)
-    
     if x>y:
         parent[x] = y
     else:
-        parent[y]=x
+        parent[y] = x
 
-
-tree = [i for i in range(N+2)]
-for i in range(2, N+1):
+tree =[0]*200001
+for i in range(2, N + 1):
     tree[i] = int(input())
 query = [list(map(int, input().split())) for _ in range(Q + N - 1)]
 ans = []
@@ -32,12 +29,11 @@ while query:
     if q[0] == 0:
         node = q[1]  # q[1]과 q[1]의 부모사이 간선생성
         union(node, tree[node])
-        
     else:
         a, b = q[1], q[2]  # a,b사이의 경로 질의
         if find(a) == find(b):
-            ans.append('YES')
+            ans.append("YES")
         else:
-            ans.append('NO')
+            ans.append("NO")
 while ans:
     print(ans.pop())
